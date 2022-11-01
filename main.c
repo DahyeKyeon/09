20d7e0102f52bfaf8f650855bb7ce718d293ada2
DahyeKyeon <113089924+DahyeKyeon@users.noreply.github.com>
2022-11-01 23:23:40 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
-#define SIZE 4
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void square_array(int  a[], int size);
-void print_array(int a[], int size);
+int main(void)
+{	int i, j;
 
-int main(void) 
-{
-	int list[SIZE] = {1, 2, 3, 4};
-	
-	print_array(list, SIZE);
-	square_array(list, SIZE);
-	print_array(list, SIZE);
+	int A[3][3]={
+		{2, 3, 0},
+		{8, 9, 1},
+		{7, 0, 5}};
+	int B[3][3]={
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1}};
+		
+	for(i=0; i<3; i++){
+		for(j=0; j<3; j++){
+			
+			int C[3][3];
+			
+			C[i][j] = A[i][j] + B[i][j];
+			
+			printf("%d\t", C[i][j]);
+		}
+		printf("\n");
+	}
 	
 	return 0;
+			
 }
+
